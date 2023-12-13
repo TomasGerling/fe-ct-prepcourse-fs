b21c208d873eb80c3a3dc13894afab71f96633cb
TomasGerling <tomasgerling@hotmail.com>
2023-12-13 20:44:10 -0300

    Realizado todos los ejercicios pasando los test

--- 06-07-08.js
+++ 06-07-08.js
@@ -11,7 +11,10 @@ function crearClasePersona() {
       // Inicializar las propiedades de la persona con los valores recibidos como argumento
 
       // Tu código aca:
-
+        this.nombre = nombre;
+        this.edad = edad;
+        this.hobbies = hobbies;
+        this.amigos = amigos;
     }
 
     addFriend(nombre, edad) {
@@ -20,6 +23,8 @@ function crearClasePersona() {
       // No debe retornar nada.
 
       // Tu código aca:
+      const nuevoAmigo = {nombre, edad};
+      this.amigos.push(nuevoAmigo)
 
     }
 
@@ -28,7 +33,7 @@ function crearClasePersona() {
       // No debe retornar nada.
 
       // Tu código aca:
-
+      this.hobbies.push(hobby);
     }
     getFriends() {
       // El método 'getFriends' debe retornar un arreglo con sólo los nombres del arreglo de amigos
@@ -38,7 +43,7 @@ function crearClasePersona() {
       // persona.getFriends() debería devolver ['martin', 'toni']
 
       // Tu código aca:
-
+      return this.amigos.map(amigo => amigo.nombre);
     }
 
     getHobbies() {
@@ -47,7 +52,7 @@ function crearClasePersona() {
       // persona.getHobbies() debe devolver ['correr', 'dormir', 'nadar']
 
       // Tu código aca:
-
+      return this.hobbies;
     }
 
     getPromedioEdad() {
@@ -66,7 +71,15 @@ function crearClasePersona() {
       // persona.getPromedioEdad() debería devolver 29 ya que (33 + 25) / 2 = 29
 
       // Tu código aca:
+      if (this.amigos && this.amigos.length > 0) {
+        const sumaEdades = this.amigos.reduce((total, amigo) => total + amigo.edad, 0);
 
+        const promedioEdad = sumaEdades / this.amigos.length;
+
+        return promedioEdad;
+      } else {
+        return null;
+      }
     }
   };
 
